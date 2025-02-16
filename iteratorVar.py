533199baf2d88c70efb8cb1cc743704595988fc7
dhkim0901 <kimvj200@gmail.com>
2025-02-16 16:04:50 +0900

    update

--- iteratorVar.py
+++ iteratorVar.py
@@ -1,16 +1,24 @@
 import itertools
 import csv
 
-regions = ["us-east-1","us-east-2","us-west-1","us-west-2","af-south-1","ap-east-1",
-"ap-south-2","ap-southeast-3","ap-southeast-4","ap-southeast-5","ap-south-1",
-"ap-northeast-3","ap-northeast-2","ap-southeast-1","ap-southeast-2",
-"ap-northeast-1","ca-central-1","ca-west-1","eu-central-1","eu-west-1",
-"eu-west-2","eu-south-1","eu-west-3","eu-south-2","eu-north-1","eu-central-2",
-"il-central-1","me-south-1","me-central-1","sa-east-1"]
+#regions = ["us-east-1","us-east-2","us-west-1","us-west-2","af-south-1","ap-east-1",
+#"ap-south-2","ap-southeast-3","ap-southeast-4","ap-southeast-5","ap-south-1",
+#"ap-northeast-3","ap-northeast-2","ap-southeast-1","ap-southeast-2",
+#"ap-northeast-1","ca-central-1","ca-west-1","eu-central-1","eu-west-1",
+#"eu-west-2","eu-south-1","eu-west-3","eu-south-2","eu-north-1","eu-central-2",
+#"il-central-1","me-south-1","me-central-1","sa-east-1"]
 
+regions = ["ap-northeast-1","ap-northeast-2","ap-northeast-3"]
+
+#r5, r6i, r6g, t3, t4g, r7
 instance_types = [
-    "db.r6i.large", "db.r6i.xlarge", "db.r6i.2xlarge", "db.r6i.4xlarge",
-    "db.r6i.8xlarge", "db.r6i.12xlarge", "db.r6i.16xlarge", "db.r6i.24xlarge", "db.r6i.32xlarge"
+    "db.t4g.micro","db.t4g.small","db.t4g.medium","db.t4g.large","db.t4g.xlarge","db.t4g.2xlarge",
+    "db.t3.micro","db.t3.small","db.t3.medium","db.t3.large","db.t3.xlarge","db.t3.2xlarge","db.r5.large",
+    "db.r5.xlarge","db.r5.2xlarge","db.r5.4xlarge","db.r5.8xlarge","db.r5.12xlarge","db.r5.16xlarge",
+    "db.r5.24xlarge","db.r6i.large","db.r6i.xlarge","db.r6i.2xlarge","db.r6i.4xlarge","db.r6i.8xlarge",
+    "db.r6i.12xlarge","db.r6i.16xlarge","db.r6i.24xlarge","db.r6i.32xlarge","db.r6g.large","db.r6g.xlarge",
+    "db.r6g.2xlarge","db.r6g.4xlarge","db.r6g.8xlarge","db.r6g.12xlarge","db.r6g.16xlarge","db.r7g.large",
+    "db.r7g.xlarge","db.r7g.2xlarge","db.r7g.4xlarge","db.r7g.8xlarge","db.r7g.12xlarge","db.r7g.16xlarge"
 ]
 database_engines = ["Aurora MySQL", "Aurora PostgreSQL"]
 models = ["Standard", "IOOptimized"]
